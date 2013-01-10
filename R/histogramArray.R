e9ae0789aeb76116fd252b2b4637c23d5bd9da5c
David Banks <dban021@aucklanduni.ac.nz>
2013-01-10 15:36:40 +1300

    Initial qqnorm/histogram array functions
    Even with normal errors in the response, residuals from a fit might not
    be true normal. These functions plot arrays of qqplots or histograms
    alongside the original qqplot / histogram to aid in determining how
    non-normal the data really is.

---
+++ R/histogramArray.R
@@ -0,0 +1,52 @@
+histogramArray = function(x, n = 7) {
+  ## Assumes lm model (gaussian errors only)
+  n = min(n, 11)
+  nRows = 2
+  nCols = ceiling(round(n + 1) / nRows)
+  opar = par(mfrow = c(2, nCols))
+  on.exit(par(opar))
+  
+  r = residuals(x)
+  h = hist(r, plot = FALSE)
+  xlab <- "Residuals"
+  mx <- mean(r)
+  sx <- sd(r)
+  rx <- range(r)
+  xmin <- min(rx[1], mx - 3.5 * sx, h$breaks[1])
+  xmax <- max(rx[2], mx + 3.5 * sx, h$breaks[length(h$breaks)])
+  ymax <- max(h$density, dnorm(mx, mx, sx)) * 1.05
+  hist(r, prob = TRUE, ylim = c(0, ymax), xlim = c(xmin, xmax),
+       xlab = xlab, col = "light blue", main = NULL)
+  mtext("Original data", 3, font = 2, col = "navy", line = 1)
+  box()
+  x1 <- seq(xmin, xmax, length = 100)
+  y1 <- dnorm(x1, mx, sx)
+  lines(x1, y1, lwd = 1.5, lty = 3)
+  
+  newdat = dat
+  response = rownames(attr(x$terms, "factors"))[1]
+  newcall = modifyModelCall(x, "newdat")
+  
+  n.obs = nrow(x$model)
+  for (i in 1:n) {
+    rNormal = rnorm(n.obs, sd = summary(x)$sigma)
+    newdat[, response] = x$fitted.values + rNormal
+    newlm = eval(parse(text = newcall))
+    r = residuals(newlm)
+    h = hist(r, plot = FALSE)
+    mx = mean(r)
+    sx = sd(r)
+    rx = range(r)
+    xmin <- min(rx[1], mx - 3.5 * sx, h$breaks[1])
+    xmax <- max(rx[2], mx + 3.5 * sx, h$breaks[length(h$breaks)])
+    ymax <- max(h$density, dnorm(mx, mx, sx)) * 1.05
+    hist(r, prob = TRUE, ylim = c(0, ymax), xlim = c(xmin, xmax),
+         xlab = xlab, col = "light blue", main = NULL)
+    mtext(paste("Normal errors: sample", i), 3, font = 2,
+          col = "navy", line = 1)
+    box()
+    x1 <- seq(xmin, xmax, length = 100)
+    y1 <- dnorm(x1, mx, sx)
+    lines(x1, y1, lwd = 1.5, lty = 3)
+  }
+}
---
+++ R/qqplotArray.R
@@ -0,0 +1,49 @@
+qqplotArray = function(x, n = 7) {
+  ## Assumes lm model (gaussian errors only)
+  n = min(n, 11)
+  nRows = 2
+  nCols = ceiling(round(n + 1) / nRows)
+  opar = par(mfrow = c(2, nCols))
+  on.exit(par(opar))
+
+  dropInf <- function(x, h) {
+    if (any(isInf <- h >= 1)) {
+      warning("Not plotting observations with leverage one:\n  ",
+              paste(which(isInf), collapse = ", "), call. = FALSE)
+      x[isInf] <- NaN
+    }
+    x
+  }
+  hii = lm.influence(x, do.coef = FALSE)$hat
+  r = residuals(x)
+  s = sqrt(deviance(x)/df.residual(x))
+  rs = dropInf(r/(s * sqrt(1 - hii)), hii)
+  ylim = range(rs, na.rm = TRUE)
+  ylim[2] = ylim[2] + diff(ylim) * 0.075
+  qq = normCheck(rs, xlab = "Theoretical Quantiles",
+                 ylab = "Sample Quantiles", ylim = ylim)
+  mtext("Original data", 3, font = 2, col = "navy", line = 1)
+  # if (id.n > 0)
+  #   text.id(qq$x[show.rs], qq$y[show.rs], show.rs)
+     
+  newdat = dat
+  response = rownames(attr(x$terms, "factors"))[1]
+  newcall = modifyModelCall(x, "newdat")
+    
+  n.obs = nrow(x$model)
+  for (i in 1:n) {
+    rNormal = rnorm(n.obs, sd = summary(x)$sigma)
+    newdat[, response] = x$fitted.values + rNormal
+    newlm = eval(parse(text = newcall))
+    r = residuals(newlm)
+    s = sqrt(deviance(newlm)) / df.residual(newlm)
+    hii <- lm.influence(newlm, do.coef = FALSE)$hat
+    rs = dropInf(r/(s * sqrt(1 - hii)), hii)
+    ylim <- range(rs, na.rm = TRUE)
+    ylim[2] <- ylim[2] + diff(ylim) * 0.075
+    normCheck(rs, xlab = "Theoretical Quantiles",
+              ylab = "Sample Quantiles", ylim = ylim)
+    mtext(paste("Normal errors: sample", i), 3, font = 2,
+          col = "navy", line = 1)
+  }
+}
