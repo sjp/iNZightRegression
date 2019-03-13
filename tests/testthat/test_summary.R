fb159dfe572b3a7600d045de705c6421d541250f
Tom Elliott <tomelliottnz@gmail.com>
2019-03-13 14:13:44 +1300

    Imrove test

--- tests/testthat/test_summary.R
+++ tests/testthat/test_summary.R
@@ -5,5 +5,8 @@ dat$y <- rbinom(100, 1, log(dat$x*2 + 1) / log(3))
 
 test_that("Probit models supported", {
     lm.probit <- glm(y ~ x, family = binomial(link = "probit"), data = dat)
-    expect_output(iNZightSummary(lm.probit))
+    smry <- capture.output(iNZightSummary(lm.probit))
+    expect_equal("Coefficients:", smry[1])
+    expect_equal(sum(grepl("AIC", smry)), 1)
+    expect_equal(sum(grepl("Fisher Scoring iterations", smry)), 1)
 })
