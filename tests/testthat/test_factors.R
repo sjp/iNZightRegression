f800a9e353c55331e0ebb3ad35137c6488b36463
Tom Elliott <tomelliottnz@gmail.com>
2019-07-25 20:21:41 +1200

    Add some temp code

--- tests/testthat/test_factors.R
+++ tests/testthat/test_factors.R
@@ -5,3 +5,21 @@ fit <- lm(Sepal.Length ~ Species, data = iris)
 # test_that("Factor comparisons", {
     
 # })
+
+# summary(f1, contrasts = )
+
+# fit <- lm(Sepal.Length ~ Sepal.Width + Species, 
+#     data = iris,
+#     contrasts = list(
+#         Species = rbind(
+#             c(0,0,-1),
+#             c(1,0,0),
+#             c(0,1,1)
+#             # c(1, 0,0,0),
+#             # c( 1,  0, 0),
+#             # c( 0,  1,  1)
+#         )
+#     )
+# )
+# summary(fit)
+# summary(f2)
