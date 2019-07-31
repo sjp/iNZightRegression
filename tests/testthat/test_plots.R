0d45abc6544a830d4a6ab5f3777e55196fee252c
Tom Elliott <tomelliottnz@gmail.com>
2019-07-31 19:42:01 +1200

    Use set seed to reduce artifical ci failure

--- tests/testthat/test_plots.R
+++ tests/testthat/test_plots.R
@@ -22,10 +22,9 @@ test_that("Plot works for basic lm", {
     expect_silent(plotlm6(lm1, which = 6))
 })
 
+set.seed(100)
 dat <- data.frame(x = runif(100, 0, 1))
 dat$y <- rbinom(100, 1, log(dat$x*2 + 1) / log(3))
-
-
 test_that("Plot works for basic glm", {
     glm0 <- glm(y ~ 1, data = dat)
     expect_silent(plotlm6(glm0, which = 1))
