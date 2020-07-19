ec8aa8dcf0937e4fff08d230fa97e25a2724aab0
Tom Elliott <tomelliottnz@gmail.com>
2020-07-19 19:24:39 +1200

    Use GHactions

---
+++ .github/install_deps.R
@@ -0,0 +1,10 @@
+# R script
+github_deps <- c(
+    "iNZightVIT/iNZightPlots@release/2.12"
+)
+
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+
+remotes::install_github(github_deps)
+remotes::install_deps(dependencies = TRUE)
+remotes::install_cran("rcmdcheck")
