083a8d7b81fc8b3b987550e5fd0f8bfce860af2f
Tom Elliott <tomelliottnz@gmail.com>
2020-07-20 08:31:06 +1200

    Framework for funciton

---
+++ R/inzplot.R
@@ -0,0 +1,16 @@
+#' Plot Method for Regression Models
+#'
+#' @param fit a regression model
+#' @param which the type of plot to draw
+#' @return A ggplot object
+#'
+#' @md
+#' @importFrom iNZightPlots inzplot
+#' @author Tom Elliott
+#' @export
+#' @examples
+#' iris_fit <- lm(Sepal.Width ~ Sepal.Length, data = iris)
+#' inzplot(iris_fit)
+inzplot.lm <- function() {
+
+}
