a73dacfa243bcc24e69230bbfd6d268afe3a01fc
Tom Elliott <tomelliottnz@gmail.com>
2020-07-25 20:13:04 +1200

    Fix CI deps install

--- .github/install_deps.R
+++ .github/install_deps.R
@@ -5,22 +5,10 @@ github_deps <- c(
     "iNZightVIT/iNZightMR@2.2.5"     # dependency of iNZightPlots
 )
 
-options(repos = c(CRAN = "https://cloud.r-project.org"))
-
-if (.Platform$OS.type == "windows") {
-    utils::install.packages("XML")
-    if (!requireNamespace("XML")) {
-        download.file(
-            sprintf(
-                "https://cran.r-project.org/bin/windows/contrib/%s/XML_3.99-0.3.zip",
-                paste(strsplit(as.character(getRversion()), "\\.")[[1]][1:2], collapse = ".")
-            ),
-            "xml.zip"
-        )
-        unzip("xml.zip", exdir = .libPaths()[1])
-        unlink("xml.zip")
-    }
-}
+options(
+    repos = c(CRAN = "https://cloud.r-project.org"),
+    install.packages.compile.from.source = "never"
+)
 
 remotes::install_github(github_deps)
 remotes::install_deps(dependencies = TRUE)
