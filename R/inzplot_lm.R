da2b8b83f071f14dfdb5c3c971efca90bc06ed71
Tom Elliott <tomelliottnz@gmail.com>
2020-09-17 10:32:53 +1200

    Add cooks contours

--- R/inzplot_lm.R
+++ R/inzplot_lm.R
@@ -15,6 +15,8 @@
 #' @param label.id integer for the number of extreme points to label (with row id)
 #' @param col.smooth the colour of smoothers
 #' @param col.bs the colour of bootstrap (smoothers)
+#' @param cook.levels levels of the Cook's distance at which to draw contours.
+#' @param col.cook the colour of Cook's distance contours
 #' @param ... additional arguments
 #' @param env the environment for evaluating things (e.g., bootstraps)
 #' @return A ggplot object
@@ -42,6 +44,8 @@ inzplot.lm <- function(x,
                        label.id = 3L,
                        col.smooth = "orangered",
                        col.bs = "lightgreen",
+                       cook.levels = c(0.5, 1),
+                       col.cook = "pink",
                        ...,
                        env = parent.frame()
                        ) {
@@ -52,18 +56,28 @@ inzplot.lm <- function(x,
     p <- switch(which,
         "residual" = ,
         "scale" = ,
-        "leverage" = .inzplot_lm_scatter(x, which, show.bootstraps, label.id, col.smooth, col.bs, ..., env = env)
+        "leverage" = .inzplot_lm_scatter(x, which, show.bootstraps,
+            label.id, col.smooth, col.bs, cook.levels, col.cook,
+            ...,
+            env = env
+        )
     )
 
     grDevices::dev.hold()
     on.exit(grDevices::dev.flush())
-    print(p)
+    suppressWarnings(print(p))
 
     invisible(p)
 }
 
-.inzplot_lm_scatter <- function(x, which, show.bootstraps, label.id, col.smooth, col.bs, ..., env) {
-    d_fun <- function(x, which, label.id = 0L) {
+.inzplot_lm_scatter <- function(x, which, show.bootstraps, label.id,
+                                col.smooth, col.bs,
+                                cook.levels, col.cook,
+                                ...,
+                                env) {
+    HEX_THRESHOLD <- 1e5
+
+    d_fun <- function(x, which, label.id = 0L, is.bs = FALSE) {
         # point labels
         if (label.id > 0L)
             iid <- seq_len(label.id)
@@ -100,7 +114,13 @@ inzplot.lm <- function(x,
                     l <- order(-cook)[iid]
                     labs[l] <- names(rp)[l]
                 }
-                data.frame(x = xx, y = rsp, lab = labs)
+                d <- data.frame(x = xx, y = rsp, lab = labs)
+
+                if (!is.bs && length(cook.levels)) {
+                    r.hat <- range(hii, na.rm = TRUE)
+                    attr(d, "r.hat") <- r.hat
+                }
+                d
             }
         )
     }
@@ -127,24 +147,75 @@ inzplot.lm <- function(x,
             title
         )
     }
+    if (which == "leverage" && !is.null(attr(d, "r.hat"))) {
+        title <- sprintf("%s, and <span style='color:%s'>Cook's contours</span>",
+            title,
+            col.cook
+        )
+    }
 
-    p <- ggplot(d, aes_(~x, ~y)) +
-        geom_point() +
+    USE_HEX <- nrow(d) > HEX_THRESHOLD
+    XL <- extendrange(range(d$x, na.rm = TRUE))
+    YL <- extendrange(range(d$y, na.rm = TRUE), f = 0.08)
+
+    p <- ggplot(d, aes_(~x, ~y))
+
+    yax2 <- NULL
+    if (which == "leverage" && !is.null(attr(d, "r.hat"))) {
+        px <- length(coef(x))
+        r.hat <- attr(d, "r.hat")
+        hh <- seq.int(min(r.hat[1L], r.hat[2L] / 100), XL[2],
+            length.out = 101)
+
+        yax2 <- numeric()
+        for (crit in cook.levels) {
+            cl.h <- sqrt(crit * px * (1 - hh) / hh)
+            yax2 <- c(yax2,
+                structure(cl.h[length(cl.h)] * c(1, -1), .Names = c(crit, crit))
+            )
+            dx <- data.frame(x = hh, y = cl.h)
+            p <- p +
+                geom_path(lty = 2, col = col.cook,
+                    data = dx, na.rm = TRUE) +
+                geom_path(aes(y = -y), lty = 2, col = col.cook,
+                    data = dx, na.rm = TRUE) +
+                geom_vline(xintercept = XL[2])
+        }
+        yax2 <- yax2[dplyr::between(yax2, YL[1], YL[2])]
+    }
+
+    if (USE_HEX) {
+        p <- p + geom_hex() +
+            scale_fill_gradient(low = "gray80", high = "black") +
+            labs(fill = "Count")
+    } else {
+        p <- p + geom_point()
+    }
+
+    p <- p +
         geom_hline(yintercept = 0, lty = 3) +
         scale_x_continuous(
             switch(which,
                 "residual" = "Fitted values",
                 "scale" = "Fitted values",
                 "leverage" = "Leverage"
-            ),
-            limits = range(d$x)
+            )
         ) +
         scale_y_continuous(
             switch(which,
                 "residual" = "Residuals",
                 "scale" = expression(sqrt(abs("Standardized residuals"))),
                 "leverage" = "Residuals"
-            )
+            ),
+            sec.axis =
+                if (!is.null(yax2) && length(yax2)) {
+                    sec_axis(
+                        trans = ~.,
+                        name = NULL,
+                        breaks = yax2,
+                        labels = names(yax2)
+                    )
+                } else waiver()
         ) +
         ggtitle(
             title,
@@ -156,7 +227,8 @@ inzplot.lm <- function(x,
         theme(
             plot.title.position = "plot",
             plot.title = element_markdown()
-        )
+        ) +
+        coord_cartesian(xlim = XL, ylim = YL, expand = FALSE)
 
     if (label.id > 0L && !is.null(d$lab)) {
         p <- p +
@@ -167,7 +239,7 @@ inzplot.lm <- function(x,
 
     if (show.bootstraps) {
         for (i in seq_along(bs.fits)) {
-            ds <- d_fun(bs.fits[[i]], which)
+            ds <- d_fun(bs.fits[[i]], which, is.bs = TRUE)
             p <- p +
                 geom_smooth(
                     data = ds,
