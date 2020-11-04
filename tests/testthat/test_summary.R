72c4a421b19ac7aa9a97e5ae2a60915614827a75
Tom Elliott <tom.elliott@auckland.ac.nz>
2020-11-05 12:08:51 +1300

    Use normal if()

--- tests/testthat/test_summary.R
+++ tests/testthat/test_summary.R
@@ -93,6 +93,6 @@ test_that("Cox PH models are supported", {
     expect_match(smry.cox, "Coefficients:", all=FALSE)
     expect_match(smry.cox, "^sex", all = FALSE)
 
-    skip_if(is.null(fit.cox$concordance))
-    expect_match(smry.cox, "^Concordance", all = FALSE)
+    if (!is.null(fit.cox$concordance))
+        expect_match(smry.cox, "^Concordance", all = FALSE)
 })
